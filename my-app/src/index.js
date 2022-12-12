617158021e6144bfa0e34ab00acacbdc3835e46f
[min02choi] <min02choi@naver.com>
2022-12-12 11:41:53 +0900

    [DT:2022-12-12] FinalTerm Study

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const close = 18;
+let today = new Date();
+let hours = today.getHours();
+let text = "GoodBye~ Closed";
+if (hours < close) {
+    text = "Now Opened!";
+}
+
+function Myfunc() {
+    return(
+        <p>{text}</p>
+    );
+}
+
+function Artist(props) {
+    return(
+        <h3>{props.name} is the Best Artist!</h3>
+    );
+}
+
+function Best(props) {
+    return(
+        <>
+            <p>Who is the Best Singer?</p>
+            <Artist name={props.who}/>
+        </>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App />
+        <Myfunc/>
+        <Best who="iKON"/>
     </React.StrictMode>
 );
